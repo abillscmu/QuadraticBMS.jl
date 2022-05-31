7143e8d4ad00579287492863d39d2252bbc0b94d
Alexander Bills <abills@andrew.cmu.edu>
2022-05-31 10:54:13 -0400

    add normalized dt

--- scripts/fullScript_mat.jl
+++ scripts/fullScript_mat.jl
@@ -28,18 +28,20 @@ h = 0.36    # Cooling Coefficient
 T_amb = 298    # Ambient Temperature
 cpm = 1587    # Thermal Capacity
 ic = [0.05,0,300]
-N=4000
+N=2000
 τ_ohm = 1
 
-N_reftraj = 10
+N_reftraj = 250
 dt_reftraj=1.0
-
-chatter_weight = 
-time_weight = 
+#WORKS but not great
+#chatter_weight = 1/10
+#time_weight = 100
+chatter_weight=1/10
+time_weight=1
 
 #Build Controller
 controller_t = QuadraticBMS.buildController(ic,capacity,N,Q_OCV,q_OCV,Q_COCV,q_COCV,Q_NOP,q_NOP,Q_POP,q_POP,Q_OOP,q_OOP,h,cpm,τ_ohm;max_T=333,pl_tol=0,top_SOC=0.99,chatter_weight=chatter_weight,time_weight=time_weight)
-
+controller_t_normalized = QuadraticBMS.buildControllerNormalized(ic,capacity,N,Q_OCV,q_OCV,Q_COCV,q_COCV,Q_NOP,q_NOP,Q_POP,q_POP,Q_OOP,q_OOP,h,cpm,τ_ohm;max_T=333,pl_tol=0,top_SOC=0.99,chatter_weight=chatter_weight,time_weight=time_weight)
 controller_reftraj = QuadraticBMS.buildController(ic,capacity,dt_reftraj,N_reftraj,Q_OCV,q_OCV,Q_COCV,q_COCV,Q_NOP,q_NOP,Q_POP,q_POP,Q_OOP,q_OOP,h,cpm,τ_ohm)
 set_silent(controller_reftraj)
 function eval_controller_t()
--- src/controller.jl
+++ src/controller.jl
@@ -129,7 +129,78 @@ function buildController(ic,capacity,N,Q_OCV,q_OCV,Q_POCV,q_POCV,Q_NOP,q_NOP,Q_P
         #Restrict to 10C
         @constraint(model,I[i]<=0)
         @constraint(model,I[i]>=-8*oneC)
-        @constraint(model,DT<=1)
+        @constraint(model,DT<=0.5)
+        @constraint(model,DT>=0.025)
+        fix(x[i,1],1)
+        
+    end
+    for i=2:N-1
+        @constraint(model,z[i]<=1)
+    end
+    for i=2:N
+        @constraint(model,e2[i-1]==(I[i]-I[i-1])^2)
+    end
+    #Objective Function
+    @constraint(model,z[end]>=top_SOC)
+    @NLobjective(model,Min,sum(DT for i in 1:N)*time_weight+sum(e2[i] for i in 1:N-1)*chatter_weight)
+
+    return model
+end
+
+#THIS IS DT
+function buildControllerNormalized(ic,capacity,N,Q_OCV,q_OCV,Q_POCV,q_POCV,Q_NOP,q_NOP,Q_POP,q_POP,Q_OOP,q_OOP,h,c,τ_ohm;max_T=333,pl_tol=0,top_SOC=0.9,chatter_weight=1,time_weight=1)
+    #Construct IPOPT controller
+    model = Model(Ipopt.Optimizer)
+    set_optimizer_attribute(model, "max_iter", 1000000)
+    #Build model matrices
+    @variable(model,x[i=1:N,j=1:3])
+    @variable(model,V[i=1:N])
+    @variable(model,OCV[i=1:N])
+    @variable(model,POCV[i=1:N])
+    @variable(model,POP[i=1:N])
+    @variable(model,NOP[i=1:N])
+    @variable(model,OOP[i=1:N+1])
+    @variable(model,I[i=1:N])
+    @variable(model,ipl[i=1:N])
+    @variable(model,T[i=1:N+1])
+    @variable(model,z[i=1:N+1])
+    @variable(model,DT)
+    @variable(model,DOOP[i=1:N])
+    #@variable(model,e[i=1:N])
+    @variable(model,e2[i=1:N-1])
+    T_amb = 300;
+    #t_vec = collect(range(0,step=dt,length=N))
+    #Initial conditions
+    fix(z[1],ic[1])
+    fix(OOP[1],ic[2])
+    fix(T[1],ic[3])
+    oneC = capacity/3600
+    #Establish model constraints
+    for i=1:N
+        ##MODEL CONSTRAINTS: ALGEBRAIC AND DIFFERENTIAL
+        #Algebraic Constraints
+        @constraint(model,V[i]==OCV[i]-POP[i]-NOP[i]-OOP[i])
+        @constraint(model,OCV[i]==x[i,:]'*Q_OCV*x[i,:]+q_OCV'*x[i,:])
+        @constraint(model,POCV[i]==x[i,:]'*Q_POCV*x[i,:]+q_POCV'*x[i,:])
+        @constraint(model,NOP[i]==x[i,:]'*Q_NOP*x[i,:]+q_NOP'*x[i,:])
+        @constraint(model,POP[i]==x[i,:]'*Q_POP*x[i,:]+q_POP'*x[i,:])
+        @constraint(model,ipl[i]==POCV[i]-OCV[i]+NOP[i])
+        #Differential Constraints
+        @NLconstraint(model,(OOP[i+1]-OOP[i])==DOOP[i]*DT)
+        @constraint(model,DOOP[i]==(-OOP[i]+x[i,:]'*Q_OOP*x[i,:]+q_OOP'*x[i,:])/τ_ohm)
+        @NLconstraint(model,(z[i+1]-z[i])==(-I[i]/3600)*DT)
+        @NLconstraint(model,(T[i+1]-T[i])==(-h*(T[i]-T_amb)-I[i]*oneC*(V[i]-OCV[i]))*DT/(c))
+        #Trivial Constraints (definitions)
+        @constraint(model,x[i,3]==I[i])
+        @constraint(model,x[i,2]==z[i])
+        ##CONSTRAINTS ON OPERATION
+        @constraint(model,ipl[i]>=pl_tol)
+        @constraint(model,T[i+1]<=max_T)
+        #@constraint(model,e[i]==(z[i+1]-1)^2)
+        #Restrict to 10C
+        @constraint(model,I[i]<=0)
+        @constraint(model,I[i]>=-8)
+        @constraint(model,DT<=0.5)
         @constraint(model,DT>=0.025)
         fix(x[i,1],1)
         
